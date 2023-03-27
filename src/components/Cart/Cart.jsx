9c1b995bbe2ab93ade99c8dc49620e52d1559528
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-03-27 22:54:28 +0600

    added git component & calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,21 +4,28 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   // destructuring
   // const { cart } = props;
-  console.log(cart);
 
-  let total = 0;
+  let totalPrice = 0;
+  let totalShipping = 0;
   for (const product of cart) {
-    total = total + product.price;
+    totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
+
+  // tax charge 7%
+  const tax = ((totalPrice * 7) / 100).toFixed(2);
+
+  // grand total
+  const grandTotal = totalPrice + totalShipping + tax;
 
   return (
     <div className="cart">
       <h1>Order Summary</h1>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: ${total}</p>
-      <p>Total Shipping Charge: $</p>
-      <p>Tax: $</p>
-      <h3>Grand Total: $</h3>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping Charge: ${totalShipping}</p>
+      <p>Tax: ${tax}</p>
+      <h3>Grand Total: ${grandTotal}</h3>
     </div>
   );
 };
